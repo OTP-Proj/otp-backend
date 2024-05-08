7f2d55585964c1336bb747d4b12a4d3008e3d037
JavidanAlizada <javidanalizada99@gmail.com>
2024-05-08 17:54:38 +0400

    schema name updated

--- otp/src/main/java/org/otp/otp/repository/JwtTokenRepository.java
+++ otp/src/main/java/org/otp/otp/repository/JwtTokenRepository.java
@@ -3,10 +3,12 @@ package org.otp.otp.repository;
 import org.otp.otp.model.entity.JwtTokenEntity;
 import org.otp.otp.model.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.UUID;
 
+@Repository
 public interface JwtTokenRepository extends JpaRepository<JwtTokenEntity, UUID> {
 
     List<JwtTokenEntity> findAllByUserId(UserEntity user);
