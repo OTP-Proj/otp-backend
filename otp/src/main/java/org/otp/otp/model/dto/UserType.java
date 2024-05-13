06bdf944191ccf28ecfd12156d48d1625aface74
JavidanAlizada <javidanalizada99@gmail.com>
2024-05-14 01:51:00 +0400

    removed unused logs

--- otp/src/main/java/org/otp/otp/model/dto/UserType.java
+++ otp/src/main/java/org/otp/otp/model/dto/UserType.java
@@ -19,7 +19,6 @@ public enum UserType {
     }
 
     public static UserType fromValue(String ru) {
-        System.out.println("value: " + ru);
         for (UserType userType : UserType.values()) {
             if (userType.getRu().equals(ru)) {
                 return userType;
@@ -29,7 +28,6 @@ public enum UserType {
     }
 
     public static UserType fromEnValue(String en) {
-        System.out.println("value: " + en);
         for (UserType userType : UserType.values()) {
             if (userType.getEn().equals(en)) {
                 return userType;
