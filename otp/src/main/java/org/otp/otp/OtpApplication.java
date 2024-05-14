ac040aef44dbe0613f9f934eacc44d965857c446
JavidanAlizada <javidanalizada99@gmail.com>
2024-05-14 17:59:03 +0400

    cache all implementation removed

--- otp/src/main/java/org/otp/otp/OtpApplication.java
+++ otp/src/main/java/org/otp/otp/OtpApplication.java
@@ -2,7 +2,6 @@ package org.otp.otp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
